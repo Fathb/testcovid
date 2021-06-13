26ae86a98ccd73fb833941ffb4c080ef5ddb7e31
Fathb <45653036+Fathb@users.noreply.github.com>
2021-06-13 13:13:46 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+function cek(e) {
+ e.preventDefault();
+ let demam = document.querySelector("#demam").value;
+ let pusing = document.querySelector("#pusing").value;
+ let tenggorokan = document.querySelector("#tenggorokan").value;
+ let lelah = document.querySelector("#lelah").value;
+ let bau = document.querySelector("#bau").value;
+
+ let answers = [demam,
+  pusing,
+  tenggorokan,
+  lelah,
+  bau];
+
+ var ya = [];
+ answers.forEach(a=> {
+  if (a == "Ya") {
+   ya.push(a);
+  }
+ });
+ if (ya.length > 3) {
+  alert("anda sangat beresiko terkena covid! segera hubungi dokter terdekat!");
+ } else if (ya.length == 3) {
+  alert("maka ........");
+ } else {
+  alert("maka.....");
+ }
+}
+
+let cekButton = document.querySelector("#cekButton");
+cekButton.addEventListener('click', cek);
